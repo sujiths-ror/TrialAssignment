5ab00eec5194db820b9989ef3a65143ed7d4585c
sujiths-ror <sujith.s@techversantinc.com>
2019-03-14 17:06:44 +0530

    Question update feature

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::QuestionsController < ApplicationController
 
-  before_action :set_question, only: %i(show edit)
+  before_action :set_question, only: %i(show edit update)
 
   def index
      @questions = Question.all.page(params[:page] || 1).per(10)
@@ -10,8 +10,17 @@ class Api::V1::QuestionsController < ApplicationController
 
   def edit; end
 
+  def update
+    @question.update(question_params) if @question.present?
+  end
+
   private
   def set_question
     @question = Question.find_by(id: params[:id])
   end
+
+  def question_params
+    params.require(:question).permit(:question, :priority, :teaming_stages, :appears, :question_type, :required, :conditions, :frequency, :role_id, :mapping_id)
+  end
 end
+
---
+++ app/views/api/v1/questions/update.json.jbuilder
@@ -0,0 +1,13 @@
+if @question.blank?
+  json.status 404
+  json.message 'Question not found'
+elsif @question.errors.any?
+  json.status 500
+  json.message @question.errors.full_messages
+else
+  json.status 200
+  json.message 'Success'
+  json.data do
+    json.question @question
+  end
+end
