b97af622ba96b3a9d581b3f891a15e1aa8fbea0a
sujiths-ror <sujith.s@techversantinc.com>
2019-03-14 16:18:34 +0530

    namespace added for import routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :questions do
-    collection do
-      get 'import'
-      post 'import'
-    end
+  namespace 'questions' do
+    get 'import'
+    post 'import'
   end
 
   namespace 'api' do
