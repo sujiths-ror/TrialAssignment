abd97ca81180e4b9458f5086461858ad5e97d60e
sujiths-ror <sujith.s@techversantinc.com>
2019-03-14 20:02:11 +0530

    Style and landing page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root to: 'questions#import'
+  root to: 'home#index'
 
   namespace 'questions' do
     get 'import'
