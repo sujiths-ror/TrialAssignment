e59eb05d1d477a65f4f9e773df70508e3f1cb9c4
sujiths-ror <sujith.s@techversantinc.com>
2019-03-14 16:21:23 +0530

    Edit API added for questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::QuestionsController < ApplicationController
 
-  before_action :set_question, only: %i(show)
+  before_action :set_question, only: %i(show edit)
 
   def index
      @questions = Question.all.page(params[:page] || 1).per(10)
   end
 
   def show; end
+
+  def edit; end
 
   private
   def set_question
---
+++ app/views/api/v1/questions/edit.json.jbuilder
@@ -0,0 +1,5 @@
+json.status 200
+json.message 'Success'
+json.data do
+  json.question @question
+end
